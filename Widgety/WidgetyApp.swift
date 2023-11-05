f8f556450aabd17b94a5d068502e85fe3e8b9d97
Alex Gawley <gawley@gmail.com>
2023-11-05 18:35:05 +0000

    Initial Commit

---
+++ Widgety/WidgetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetyApp.swift
+//  Widgety
+//
+//  Created by Alex Gawley on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
